84f9264a1bbd44fc9d794cb7e9158726233787ac
Mathis Gervais-Lefebvre <mgervaislefebvre@csfoy.ca>
2022-01-25 15:11:08 -0500

    Changement de la config pour faire afficher les messages d'erreurs lié à la validation

--- src/main/java/ulaval/glo2003/Main.java
+++ src/main/java/ulaval/glo2003/Main.java
@@ -3,15 +3,17 @@ package ulaval.glo2003;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
 
 import java.io.IOException;
 import java.net.URI;
 
 public class Main {
-    //Test pour discord
     public static void main(String[] args) throws IOException {
         ResourceConfig resourceConfig = new ResourceConfig()
-                .register(new SellerController());
+                .property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true)
+                .register(SellerController.class);
+
         URI uri = URI.create("http://localhost:8080/");
 
         HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, resourceConfig);
