232830f308f4872487ba7d474c4a0b3e8c32dfbd
Mathis Gervais-Lefebvre <mgervaislefebvre@csfoy.ca>
2022-01-25 15:11:24 -0500

    Utilisation des tags de validation pour sellers

--- src/main/java/ulaval/glo2003/SellerController.java
+++ src/main/java/ulaval/glo2003/SellerController.java
@@ -1,38 +1,22 @@
 package ulaval.glo2003;
 
-import jakarta.ws.rs.GET;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 
 import java.net.URI;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
-@Path("/")
+@Path("/sellers")
 public class SellerController {
 
-    private List<SellerObject> sellerList = new ArrayList<>();
-
-    @GET
-    public Response getMain(){
-        return Response.ok().build();
-    }
+    private final List<SellerDTO> sellerList = new ArrayList<>();
 
     @POST
-    @Path("/sellers/")
-    public Response postCreatingSeller(SellerObject seller) throws Exception {
-
-
-        if(seller.name.isEmpty() || seller.bio.isEmpty()){
-            throw new Exception("MISSING_PARAMETER");
-        } else if(seller.birthDate.isAfter(LocalDate.now().minusYears(18))){
-            throw new Exception("INVALID_PARAMETER");
-        }
-
+    public Response postCreatingSeller(@Valid @NotNull SellerDTO seller) {
         sellerList.add(seller);
         return Response.created(URI.create(Integer.toString(sellerList.size() - 1))).build();
     }
--- src/main/java/ulaval/glo2003/SellerDTO.java
+++ src/main/java/ulaval/glo2003/SellerDTO.java
@@ -1,14 +1,19 @@
 package ulaval.glo2003;
 
 import jakarta.json.bind.annotation.JsonbDateFormat;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.time.LocalDate;
-import java.util.Date;
 
-public class SellerObject {
+
+public class SellerDTO {
+    @NotEmpty(message = "Le champ \"name\" est obligatoire.")
     public String name;
+
+    @NotEmpty(message = "Le champ \"bio\" est obligatoire.")
     public String bio;
 
     @JsonbDateFormat(value = "yyyy-MM-dd")
+    @MinimumAge(age = 18)
     public LocalDate birthDate;
 }
