42001a3804d9808e98b668a4115b9aafdde10ffa
Mathis Gervais-Lefebvre <mgervaislefebvre@csfoy.ca>
2022-01-25 15:10:27 -0500

    Ajout du validateur d'age

---
+++ src/main/java/ulaval/glo2003/MinimumAge.java
@@ -0,0 +1,20 @@
+package ulaval.glo2003;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {AgeValidator.class})
+public @interface MinimumAge {
+    String message() default "Invalid birthdate.";
+
+    int age() default 18;
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
