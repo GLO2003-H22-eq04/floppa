aa5c10b6afd88208eac8abcea2fce6f4095cf576
Mathis Gervais-Lefebvre <mgervaislefebvre@csfoy.ca>
2022-01-30 14:00:43 -0500

    Fix type erreur et retrait tag problemetique

--- src/main/java/ulaval/glo2003/SellerController.java
+++ src/main/java/ulaval/glo2003/SellerController.java
@@ -8,6 +8,7 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import ulaval.glo2003.Validation.Errors.InvalidParameterError;
+import ulaval.glo2003.Validation.Errors.ItemNotFoundError;
 import ulaval.glo2003.Validation.Errors.MissingParameterError;
 
 import java.net.URI;
@@ -27,13 +28,13 @@ public class SellerController {
     @GET
     @Path("/{sellerId}")
     @Produces(MediaType.APPLICATION_JSON)
-    public SellerInfoResponseDTO getSeller(@NotEmpty(payload = MissingParameterError.class) @PathParam("sellerId") int sellerId) throws InvalidParameterError {
+    public SellerInfoResponseDTO getSeller(@PathParam("sellerId") int sellerId) throws ItemNotFoundError {
         var seller = sellerRepository.findById(sellerId);
         if (seller.isPresent()) {
             var sellerInfos = seller.get();
             return new SellerInfoResponseDTO(sellerId, sellerInfos.getName(), sellerInfos.getCreatedAt(), sellerInfos.getBio(), sellerInfos.getProducts());
         }
 
-        throw new InvalidParameterError("L'id fourni n'existe pas.");
+        throw new ItemNotFoundError("L'id fourni n'existe pas.");
     }
 }
