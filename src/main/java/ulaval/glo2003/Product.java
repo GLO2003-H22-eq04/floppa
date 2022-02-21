9524059c174914515506275f5f1e037f513ccb01
GabrielGagnon <gabriel.gagnon97@gmail.com>
2022-02-20 22:47:49 -0500

    Ajout de ProductCategory et ajout du squelette d'un produit

--- src/main/java/ulaval/glo2003/Product.java
+++ src/main/java/ulaval/glo2003/Product.java
@@ -1,4 +1,27 @@
 package ulaval.glo2003;
 
+import java.util.List;
+
 public class Product {
+
+    private String title;
+    private String description;
+    private double suggestedPrice;
+    private List<ProductCategory> categories;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getSuggestedPrice() {
+        return suggestedPrice;
+    }
+
+    public List<ProductCategory> getCategories() {
+        return categories;
+    }
 }
