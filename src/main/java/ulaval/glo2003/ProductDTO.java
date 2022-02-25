c52da67999ee6d627480cbbdd6397e3b4060369f
Carol-Ann Collin <carol-ann.collin.1@ulaval.ca>
2022-02-25 18:41:26 -0500

    Implémentation dto

--- src/main/java/ulaval/glo2003/ProductDTO.java
+++ src/main/java/ulaval/glo2003/ProductDTO.java
@@ -1,8 +1,29 @@
 package ulaval.glo2003;
 
+import jakarta.validation.GroupSequence;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import ulaval.glo2003.Validation.Errors.InvalidParameterError;
+import ulaval.glo2003.Validation.Errors.MissingParameterError;
+import ulaval.glo2003.Validation.HighPriority;
+import ulaval.glo2003.Validation.MinimumPrice;
+
+import java.util.List;
+
+@GroupSequence({HighPriority.class, ProductDTO.class})
 public class ProductDTO {
 
+    @NotNull(message = "Le champ \"name\" est obligatoire.", payload = MissingParameterError.class, groups = HighPriority.class)
+    @NotBlank(message = "Le champ \"name\" doit contenir une valeur.", payload = InvalidParameterError.class)
     public String title;
+
+    @NotNull(message = "Le champ \"name\" est obligatoire.", payload = MissingParameterError.class, groups = HighPriority.class)
+    @NotBlank(message = "Le champ \"name\" doit contenir une valeur.", payload = InvalidParameterError.class)
     public String description;
+
+    @NotNull(message = "Le champ \"name\" est obligatoire.", payload = MissingParameterError.class, groups = HighPriority.class)
+    @MinimumPrice(price = 1, payload = InvalidParameterError.class, message = "Prix invalide.")
     public double suggestedPrice;
+
+    public List<String> categories;
 }
---
+++ src/main/java/ulaval/glo2003/ProductSellerDTO.java
@@ -0,0 +1,23 @@
+package ulaval.glo2003;
+
+public class ProductSellerDTO {
+
+    public String id;
+    public String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
