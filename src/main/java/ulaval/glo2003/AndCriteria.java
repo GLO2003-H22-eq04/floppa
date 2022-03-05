316ddc816ae95acac21d04e0ab2ae0c81f38879b
GabrielGagnon <gabriel.gagnon97@gmail.com>
2022-03-04 22:25:14 -0500

    CriteriaCategories Fait

---
+++ src/main/java/ulaval/glo2003/AndCriteria.java
@@ -0,0 +1,10 @@
+package ulaval.glo2003;
+
+import java.util.List;
+
+public class AndCriteria implements Criteria{
+    @Override
+    public List<Product> meetCriteria(List<Product> products) {
+        return null;
+    }
+}
--- src/main/java/ulaval/glo2003/CriteriaCategories.java
+++ src/main/java/ulaval/glo2003/CriteriaCategories.java
@@ -1,10 +1,27 @@
 package ulaval.glo2003;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CriteriaCategories implements Criteria{
+    List<ProductCategory> productCategories;
+
+    public CriteriaCategories(List<ProductCategory> p_productCategories){
+        this.productCategories = p_productCategories;
+    }
     @Override
-    public List<Product> meetCriteria(List<Product> products) {
-        return null;
+    public List<Product> meetCriteria(List<Product> p_products) {
+        List<Product> sellerCategory = new ArrayList<Product>();
+
+        for(Product product : p_products){
+            for(ProductCategory productCategory : productCategories){
+                if(product.getCategories().contains(productCategory)){
+                    sellerCategory.add(product);
+                    break;
+                }
+            }
+        }
+
+        return  sellerCategory;
     }
 }
