e9dc943bdfea3a1cb4d2d77f9e80fa33181c0676
GabrielGagnon <gabriel.gagnon97@gmail.com>
2022-03-04 22:01:29 -0500

    CriteriaTitle fait

--- src/main/java/ulaval/glo2003/CriteriaTitle.java
+++ src/main/java/ulaval/glo2003/CriteriaTitle.java
@@ -1,10 +1,25 @@
 package ulaval.glo2003;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class CriteriaTitle implements Criteria{
+    String title;
+
+    public CriteriaTitle(String p_title){
+        this.title = p_title;
+    }
     @Override
-    public List<Product> meetCriteria(List<Product> products) {
-        return null;
+    public List<Product> meetCriteria(List<Product> p_products) {
+        List<Product> sellerTitle = new ArrayList<Product>();
+
+        for(Product product : p_products){
+            if(Objects.equals(product.getTitle(), title)){
+                sellerTitle.add(product);
+            }
+        }
+
+        return sellerTitle;
     }
 }
