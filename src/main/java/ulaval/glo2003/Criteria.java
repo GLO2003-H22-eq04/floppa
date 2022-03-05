69029b83b817b2db2ba9062d06e8d1a0cb725ecc
GabrielGagnon <gabriel.gagnon97@gmail.com>
2022-03-04 21:23:02 -0500

    Début du design partern criteria

---
+++ src/main/java/ulaval/glo2003/Criteria.java
@@ -0,0 +1,6 @@
+package ulaval.glo2003;
+import java.util.List;
+
+public interface Criteria {
+    public List<Product> meetCriteria(List<Product> products);
+}
---
+++ src/main/java/ulaval/glo2003/CriteriaSellerID.java
@@ -0,0 +1,17 @@
+package ulaval.glo2003;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CriteriaSellerID implements Criteria{
+    @Override
+    public List<Product> meetCriteria(List<Product> products) {
+        List<Product> sellerID = new ArrayList<Product>();
+
+        for(Product product : products){
+
+        }
+
+        return sellerID;
+    }
+}
