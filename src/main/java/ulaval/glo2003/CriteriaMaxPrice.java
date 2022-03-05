6b6cc7c39a97c919ec54e929b77212296b55c113
GabrielGagnon <gabriel.gagnon97@gmail.com>
2022-03-04 21:52:50 -0500

    CriteriaMaxPrice fait

--- src/main/java/ulaval/glo2003/CriteriaMaxPrice.java
+++ src/main/java/ulaval/glo2003/CriteriaMaxPrice.java
@@ -1,10 +1,24 @@
 package ulaval.glo2003;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CriteriaMaxPrice implements Criteria{
+    int maxPrice;
+
+    public CriteriaMaxPrice(int p_maxPrice){
+        this.maxPrice = p_maxPrice;
+    }
     @Override
-    public List<Product> meetCriteria(List<Product> products) {
-        return null;
+    public List<Product> meetCriteria(List<Product> p_products) {
+        List<Product> sellerIDList = new ArrayList<Product>();
+
+        for(Product product : p_products){
+            if(product.getSuggestedPrice().getValue() <= maxPrice){
+                sellerIDList.add(product);
+            }
+        }
+
+        return sellerIDList;
     }
 }
