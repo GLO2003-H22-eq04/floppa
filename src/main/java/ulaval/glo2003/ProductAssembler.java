429f3938659a83f083421a51ccd7a4c3004402d4
Carol-Ann Paradis <90847140+CarolannParadis@users.noreply.github.com>
2022-03-07 12:34:04 -0500

    Fix pour pr

--- src/main/java/ulaval/glo2003/ProductAssembler.java
+++ src/main/java/ulaval/glo2003/ProductAssembler.java
@@ -3,14 +3,14 @@ package ulaval.glo2003;
 public class ProductAssembler {
 
     public ProductInfoResponseDTO toDto(Product product, ProductSellerDTO productSellerDTO, OfferDTO offerDTO){
-        ProductInfoResponseDTO productDTO = new ProductInfoResponseDTO();
+        var productDTO = new ProductInfoResponseDTO();
 
         productDTO.id = String.valueOf(product.getProductId());
         productDTO.createdAt = product.getCreatedAt();
         productDTO.title = product.getTitle();
         productDTO.description = product.getDescription();
         productDTO.suggestedPrice = product.getSuggestedPrice();
-        productDTO.categories = product.getCategoriesName(product.getCategories());
+        productDTO.categories = product.getCategoriesName();
         productDTO.seller = productSellerDTO;
         productDTO.offers = offerDTO;
 
