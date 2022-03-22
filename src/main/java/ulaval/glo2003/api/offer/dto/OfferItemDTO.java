d000ff33b176fcd70fd6ef9aa5fdff94b6dcbbec
GabrielGagnon <gabriel.gagnon97@gmail.com>
2022-03-22 19:19:40 -0400

    OfferItemDTO "fait"

--- src/main/java/ulaval/glo2003/api/offer/dto/OfferItemDTO.java
+++ src/main/java/ulaval/glo2003/api/offer/dto/OfferItemDTO.java
@@ -1,4 +1,34 @@
 package ulaval.glo2003.api.offer.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import ulaval.glo2003.api.validation.HighPriority;
+import ulaval.glo2003.api.validation.errors.InvalidParameterError;
+import ulaval.glo2003.api.validation.errors.MissingParameterError;
+import ulaval.glo2003.domain.product.Amount;
+
 public class OfferItemDTO {
+
+
+    @NotNull(message = "Le champ \"name\" est obligatoire.", payload = MissingParameterError.class, groups = HighPriority.class)
+    @NotBlank(message = "Le champ \"name\" doit contenir une valeur.", payload = InvalidParameterError.class)
+    public String name;
+
+    @NotNull(message = "Le champ \"message\" est obligatoire.", payload = MissingParameterError.class, groups = HighPriority.class)
+    @NotBlank(message = "Le champ \"message\" doit contenir une valeur.", payload = InvalidParameterError.class)
+    public String message;
+
+    @NotNull(message = "Le champ \"message\" est obligatoire.", payload = MissingParameterError.class, groups = HighPriority.class)
+    @NotBlank(message = "Le champ \"message\" doit contenir une valeur.", payload = InvalidParameterError.class)
+    public String email;
+
+    @NotNull(message = "Le champ \"message\" est obligatoire.", payload = MissingParameterError.class, groups = HighPriority.class)
+    @NotEmpty(message = "Le champ \"message\" doit contenir une valeur.", payload = InvalidParameterError.class)
+    public int phoneNumber;
+
+    @NotNull(message = "Le champ \"message\" est obligatoire.", payload = MissingParameterError.class, groups = HighPriority.class)
+    @NotEmpty(message = "Le champ \"message\" doit contenir une valeur.", payload = InvalidParameterError.class)
+    public Amount amount;
+
 }
--- src/main/java/ulaval/glo2003/domain/offer/OfferItem.java
+++ src/main/java/ulaval/glo2003/domain/offer/OfferItem.java
@@ -4,37 +4,37 @@ import ulaval.glo2003.api.offer.dto.OfferItemDTO;
 import ulaval.glo2003.domain.product.Amount;
 
 public class OfferItem {
-//    private final String name;
-//    private final String email;
-//    private final int phoneNumber;
-//    private final Amount amount;
-//    private final String message;
-//
-//    public OfferItem(OfferItemDTO offer){
-//        name = offer.name;
-//        email = offer.email;
-//        phoneNumber = offer.phoneNumber;
-//        amount = offer.amount;
-//        message = offer.message;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public int getPhoneNumber() {
-//        return phoneNumber;
-//    }
-//
-//    public Amount getAmount() {
-//        return amount;
-//    }
-//
-//    public String getMessage() {
-//        return message;
-//    }
+    private final String name;
+    private final String email;
+    private final int phoneNumber;
+    private final Amount amount;
+    private final String message;
+
+    public OfferItem(OfferItemDTO offer){
+        name = offer.name;
+        email = offer.email;
+        phoneNumber = offer.phoneNumber;
+        amount = offer.amount;
+        message = offer.message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Amount getAmount() {
+        return amount;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
