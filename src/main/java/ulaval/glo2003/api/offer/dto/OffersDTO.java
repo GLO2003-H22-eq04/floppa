596a4788b54b9fcc785c4b12a8b92f53eb092554
GabrielGagnon <gabriel.gagnon97@gmail.com>
2022-03-23 04:13:11 -0400

    Ajout des nouvelles infos demandé dans le offersDto

--- src/main/java/ulaval/glo2003/api/offer/dto/OffersDTO.java
+++ src/main/java/ulaval/glo2003/api/offer/dto/OffersDTO.java
@@ -2,16 +2,26 @@ package ulaval.glo2003.api.offer.dto;
 
 import jakarta.json.bind.annotation.JsonbCreator;
 import jakarta.json.bind.annotation.JsonbProperty;
+import java.util.List;
 
 public class OffersDTO {
 
     public double mean;
-
+    public double min;
+    public double max;
     public int count;
+    public List<OfferItemResponseDTO> items;
 
     @JsonbCreator
-    public OffersDTO(@JsonbProperty("mean") double mean, @JsonbProperty("count") int count){
+    public OffersDTO(@JsonbProperty("min") double min,
+                  @JsonbProperty("max") double max,
+                  @JsonbProperty("mean") double mean,
+                  @JsonbProperty("count") int count,
+                  @JsonbProperty("items") List<OfferItemResponseDTO> items) {
+        this.min = min;
+        this.max = max;
         this.mean = mean;
         this.count = count;
+        this.items = items;
     }
 }
