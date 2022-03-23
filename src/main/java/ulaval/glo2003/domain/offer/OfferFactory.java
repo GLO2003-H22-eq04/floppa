c60d530152ea5dbf52f67f354545ccfdeb153826
GabrielGagnon <gabriel.gagnon97@gmail.com>
2022-03-23 00:26:01 -0400

    Fini le OfferFactory

--- src/main/java/ulaval/glo2003/domain/offer/OfferFactory.java
+++ src/main/java/ulaval/glo2003/domain/offer/OfferFactory.java
@@ -1,10 +1,20 @@
 package ulaval.glo2003.domain.offer;
 
 import ulaval.glo2003.api.offer.dto.OfferItemDTO;
+import ulaval.glo2003.domain.product.Amount;
 
 public class OfferFactory {
 
     public OfferItem createNewOffer(OfferItemDTO offerItemDTO){
         var offer = new OfferItem();
+        offer.setName(offerItemDTO.name);
+        offer.setEmail(offerItemDTO.email);
+        offer.setPhoneNumber(offerItemDTO.phoneNumber);
+
+        offer.setAmount(new Amount(offerItemDTO.amount));
+
+        offer.setMessage(offerItemDTO.message);
+
+        return offer;
     }
 }
