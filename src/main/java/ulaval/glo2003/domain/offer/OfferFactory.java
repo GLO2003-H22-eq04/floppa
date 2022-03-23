a749636bb71628eff068231836381e38f9d8b693
GabrielGagnon <gabriel.gagnon97@gmail.com>
2022-03-23 00:16:47 -0400

    Factory pour les offres

---
+++ src/main/java/ulaval/glo2003/domain/offer/OfferFactory.java
@@ -0,0 +1,10 @@
+package ulaval.glo2003.domain.offer;
+
+import ulaval.glo2003.api.offer.dto.OfferItemDTO;
+
+public class OfferFactory {
+
+    public OfferItem createNewOffer(OfferItemDTO offerItemDTO){
+        var offer = new OfferItem();
+    }
+}
