c1f40599b61d963864062a6f74e159d7e2fdba42
Carol-Ann Paradis <90847140+CarolannParadis@users.noreply.github.com>
2022-03-30 16:25:49 -0400

    Ajout dans le contrôleur

--- src/main/java/ulaval/glo2003/api/seller/SellerController.java
+++ src/main/java/ulaval/glo2003/api/seller/SellerController.java
@@ -6,15 +6,23 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import ulaval.glo2003.applicatif.buyer.BuyerInfoResponseDTO;
+import ulaval.glo2003.applicatif.buyer.BuyerItemResponseDTO;
+import ulaval.glo2003.applicatif.offer.OfferInfoResponseDTO;
+import ulaval.glo2003.applicatif.product.ProductOfferInfoResponseDTO;
+import ulaval.glo2003.applicatif.seller.CurrentSellerDTO;
 import ulaval.glo2003.applicatif.seller.SellerDto;
 import ulaval.glo2003.applicatif.seller.SellerInfoResponseDto;
 import ulaval.glo2003.api.validation.errors.ItemNotFoundError;
 import ulaval.glo2003.api.validation.errors.MissingParameterError;
+import ulaval.glo2003.domain.offer.Offers;
 import ulaval.glo2003.domain.product.repository.ProductRepository;
 import ulaval.glo2003.domain.seller.Seller;
 import ulaval.glo2003.domain.seller.repository.SellerRepository;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Path(SellerController.SELLERS_PATH)
@@ -22,13 +30,18 @@ public class SellerController {
 
     public static final String SELLERS_PATH = "/sellers";
     public static final String GET_SELLER_PATH = "/{sellerId}";
+    public static final String GET_CURRENT_SELLER_PATH = "/@me";
     public static final String PARAM_SELLER_ID = "sellerId";
+    public static final String SELLER_ID_HEADER = "X-Seller-Id";
 
     @Inject
     private SellerRepository sellerRepository;
 
     @Inject
     private ProductRepository productRepository;
+
+    @Inject
+    private SellerAssembler sellerAssembler;
 
     @POST
     public Response postCreatingSeller(@Valid @NotNull(payload = MissingParameterError.class) SellerDto seller) {
@@ -54,5 +67,50 @@ public class SellerController {
         }
 
         throw new ItemNotFoundError("L'id fourni n'existe pas.");
+    }
+
+    @GET
+    @Path(GET_CURRENT_SELLER_PATH)
+    @Produces(MediaType.APPLICATION_JSON)
+    public CurrentSellerDTO getCurrentSeller(@HeaderParam(SELLER_ID_HEADER) UUID sellerId){
+
+        var seller = sellerRepository.findById(sellerId).get();
+        var offerList = getProductOfferList(seller);
+
+        return sellerAssembler.currentSellerToDTO(seller, sellerId, offerList);
+    }
+
+    private List<BuyerItemResponseDTO> getOfferList(Offers offers){
+        List<BuyerItemResponseDTO> buyerList = new ArrayList<>();
+        for (var offer : offers.getListOffer()){
+            buyerList.add(new BuyerItemResponseDTO(
+                    offer.getCreatedAt(),
+                    offer.getAmount().getValue(),
+                    offer.getMessage(),
+                    new BuyerInfoResponseDTO(offer.getName(),offer.getEmail(),offer.getPhoneNumber())));
+        }
+        return buyerList;
+    }
+
+    private List<ProductOfferInfoResponseDTO> getProductOfferList(Seller seller){
+        List<ProductOfferInfoResponseDTO> productList = new ArrayList<>();
+        List<BuyerItemResponseDTO> buyerList = new ArrayList<>();
+        for (var product : seller.getProductList()){
+
+            productList.add(new ProductOfferInfoResponseDTO(
+                    product.getProductId(),
+                    product.getTitle(),
+                    product.getDescription(),
+                    product.getCreatedAt(),
+                    product.getSuggestedPrice().getValue(),
+                    product.getCategories(),
+                    new OfferInfoResponseDTO(product.getOffers().getMin(),
+                            product.getOffers().getMax(),
+                            product.getOffers().getMean(),
+                            product.getOffers().getCount(),
+                            getOfferList(product.getOffers()))));
+
+        }
+        return productList;
     }
 }
