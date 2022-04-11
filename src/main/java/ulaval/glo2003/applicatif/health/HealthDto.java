c0b3ecdf504a733818e6af595f06238c1d625617
Carol-Ann Collin <carol-ann.collin.1@ulaval.ca>
2022-04-11 18:21:12 -0400

    Changement bd -> db

--- src/main/java/ulaval/glo2003/applicatif/health/HealthDto.java
+++ src/main/java/ulaval/glo2003/applicatif/health/HealthDto.java
@@ -6,11 +6,11 @@ import jakarta.json.bind.annotation.JsonbProperty;
 public class HealthDto {
 
     public final boolean api;
-    public final boolean bd;
+    public final boolean db;
 
     @JsonbCreator
-    public HealthDto(@JsonbProperty("api") boolean api, @JsonbProperty("bd") boolean bd) {
+    public HealthDto(@JsonbProperty("api") boolean api, @JsonbProperty("db") boolean db) {
         this.api = api;
-        this.bd = bd;
+        this.db = db;
     }
 }
