3b4df6ffe5a2f0b3317faa2880d7074f7513b380
Mathis Gervais-Lefebvre <mgervaislefebvre@csfoy.ca>
2022-04-17 12:31:48 -0400

    Ajout de tests de code d'erreurs pour le seller

---
+++ src/main/java/ulaval/glo2003/applicatif/validation/ErrorDto.java
@@ -0,0 +1,13 @@
+package ulaval.glo2003.applicatif.validation;
+
+public class ErrorDto {
+    public String code;
+    public String description;
+
+    public ErrorDto(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public ErrorDto(){}
+}
