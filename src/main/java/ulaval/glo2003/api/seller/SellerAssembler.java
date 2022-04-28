7fced4c5837dda7fb46109010aa5e5520da9b666
Carol-Ann <carol-ann.paradis.1@ulaval.ca>
2022-04-28 19:05:19 -0400

    Ajout fonctions SelllerAssembler

--- src/main/java/ulaval/glo2003/api/seller/SellerAssembler.java
+++ src/main/java/ulaval/glo2003/api/seller/SellerAssembler.java
@@ -3,15 +3,20 @@ package ulaval.glo2003.api.seller;
 import ulaval.glo2003.applicatif.dto.buyer.BuyerInfoResponseDto;
 import ulaval.glo2003.applicatif.dto.buyer.BuyerItemResponseDto;
 import ulaval.glo2003.applicatif.dto.offer.OfferInfoResponseDto;
+import ulaval.glo2003.applicatif.dto.offer.OfferResponseDto;
+import ulaval.glo2003.applicatif.dto.product.ProductOfSellerDto;
 import ulaval.glo2003.applicatif.dto.product.ProductOfferInfoResponseDto;
 import ulaval.glo2003.applicatif.dto.seller.CurrentSellerDto;
+import ulaval.glo2003.applicatif.dto.seller.SellerInfoResponseDto;
 import ulaval.glo2003.domain.offer.Offers;
 import ulaval.glo2003.domain.seller.Seller;
 
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
 public class SellerAssembler {
 
@@ -26,6 +31,18 @@ public class SellerAssembler {
         currentSeller.products = getProductOfferList(seller);
 
         return currentSeller;
+    }
+
+    public SellerInfoResponseDto sellerInfoResponseToDto(Seller seller) {
+        var sellerInfo = new SellerInfoResponseDto();
+
+        sellerInfo.id = seller.getId();
+        sellerInfo.name = seller.getName();
+        sellerInfo.bio = seller.getBio();
+        sellerInfo.createdAt = seller.getCreatedAt();
+        sellerInfo.products = getSellerProductList(seller);
+
+        return sellerInfo;
     }
 
     public List<BuyerItemResponseDto> getOfferList(Offers offers) {
@@ -55,6 +72,25 @@ public class SellerAssembler {
                             product.getOffers().getMean(),
                             product.getOffers().getCount(),
                             getOfferList(product.getOffers()))));
+        return productList;
+    }
+
+    private Collection<ProductOfSellerDto> getSellerProductList(Seller seller) {
+        Collection<ProductOfSellerDto> productList = new ArrayList<>();
+
+        for (var product : seller.getProducts()) {
+
+            productList.add(new ProductOfSellerDto(
+                    product.getProductId(),
+                    product.getCreatedAt(),
+                    product.getTitle(),
+                    product.getDescription(),
+                    product.getSuggestedPrice().getValue(),
+                    product.getCategoriesName(),
+                    new OfferResponseDto(product.getOffers().getMean(),
+                            product.getOffers().getCount())));
+
+        }
         return productList;
     }
 
